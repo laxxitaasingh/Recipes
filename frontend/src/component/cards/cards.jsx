83b04e42236facf7ddd7e26438437daf3f0fcfbc
laxxitaasingh <laxitasingh2112@gmail.com>
2024-07-30 15:59:52 +0530

    updating data through db

--- frontend/src/component/cards/cards.jsx
+++ frontend/src/component/cards/cards.jsx
@@ -1,4 +1,4 @@
-import * as React from 'react';
+import axios from "axios";
 import './cards.css'
 import Card from '@mui/material/Card';
 import CardActions from '@mui/material/CardActions';
@@ -6,8 +6,32 @@ import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
+import React, { useState, useEffect } from "react";
 
 function MediaCard() {
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+ 
+  useEffect(() => {
+    // API endpoint
+    const apiUrl = "http://localhost:3000/collections";
+
+    // Fetch data from the API using axios
+    axios
+      .get(apiUrl)
+      .then((response) => {
+        setData(response.data); // Assuming response.data contains the array of items
+        setLoading(false);
+        console.log(response.data.ingredients)
+        // console.log(JSON.parse(response.data.ingredients))
+        console.log('dddd', response.data)
+      })
+      .catch((error) => {
+        setError(error);
+        setLoading(false);
+      });
+  }, []);
   return (
     <Card sx={{ maxWidth: 345 }}>
       <CardMedia
@@ -17,11 +41,10 @@ function MediaCard() {
       />
       <CardContent>
         <Typography gutterBottom variant="h5" component="div">
-          Lizard
+          {data.name}
         </Typography>
         <Typography variant="body2" color="text.secondary">
-          Lizards are a widespread group of squamate reptiles, with over 6,000
-          species, ranging across all continents except Antarctica
+          {data.data}
         </Typography>
       </CardContent>
       <CardActions>
