4d42f1d64292b4f3b69141b139bdda4de1e1b8c2
laxxitaasingh <laxitasingh2112@gmail.com>
2024-08-02 16:04:26 +0530

    addding material ui

--- frontend/src/component/header/header.jsx
+++ frontend/src/component/header/header.jsx
@@ -1,5 +1,25 @@
-import React from "react";
-import "./header.css";
+// import React from "react";
+// import "./header.css";
+
+// function Header() {
+//   return (
+//     <>
+//       <div className="heading">
+//         <h1>Header</h1>
+//         <div className="buttons">
+//           <button>SignUp</button>
+//           <button>Login</button>
+//         </div>
+//       </div>
+//       <div>Hello</div>
+//     </>
+//   );
+// }
+
+// export default Header;
+import React from 'react';
+import { Button } from '@mui/material';
+import './header.css';
 
 function Header() {
   return (
@@ -7,8 +27,20 @@ function Header() {
       <div className="heading">
         <h1>Header</h1>
         <div className="buttons">
-          <button>SignUp</button>
-          <button>Login</button>
+          <Button 
+            variant="contained" 
+            color="primary"
+            sx={{ borderRadius: '20px', padding: '10px 20px' }}
+          >
+            SignUp
+          </Button>
+          <Button 
+            variant="contained" 
+            color="primary"
+            sx={{ borderRadius: '20px', padding: '10px 20px' }}
+          >
+            Login
+          </Button>
         </div>
       </div>
       <div>Hello</div>
